23221a70d5a08d6a4c2bf22438650fc51e8d0cb3
maryalenn <maryachuong@gmail.com>
2022-01-19 21:12:49 -0800

    Completed Javascript For Loops Exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,16 @@
+// create your loops here.
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log('Next Loop Exercise');
+
+for (let i = 0; i < 10; i + (i = i + 2)) {
+  console.log(i);
+}
+console.log('"Time till explosion" Exercise');
+
+for (let i = 100; i >= 0; i--) {
+  console.log('Time till explosion: ' + i + '!');
+}
